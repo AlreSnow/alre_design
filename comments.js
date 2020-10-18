946d5d12f6a88b4ae4f820afd0296415d83a2392
Alre Snow <32583273+AlreSnow@users.noreply.github.com>
2020-10-18 16:46:05 -0400

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,29 @@
+let commentsForm = document.querySelector('.comments-form');
+let commentsList = document.querySelector('.comments-list');
+
+let inputName = document.querySelector('.comments-form-name-field');
+let inputText = document.querySelector('.comments-form-comment');
+let inputEmail = document.querySelector('.comments-form-email-field');
+
+commentsForm.onsubmit = function (evt) {
+	evt.preventDefault();
+
+	let newComment = document.createElement('li');
+	newComment.classList.add('comments-item');
+
+	let authorName = document.createElement('p');
+	authorName.textContent = inputName.value;
+	authorName.classList.add('author');
+
+	let commentText = document.createElement('p');
+	commentText.textContent = inputText.value;
+	commentText.classList.add('comment');
+
+	commentsList.append(newComment);
+	newComment.append(authorName);
+	newComment.append(commentText);
+
+	inputName.value = '';
+	inputText.value = '';
+	inputEmail.value = '';
+}
